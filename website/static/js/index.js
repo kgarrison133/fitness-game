aaac83e06ad6769580f8ea78bf46a1eb7bd8c22f
Kenneth <kgarrison13@mail.valenciacollege.edu>
2023-10-07 22:51:32 -0400

    added character img

---
+++ website/static/js/index.js
@@ -0,0 +1,49 @@
+const myCanvas = document.getElementById("myCanvas");
+const c = myCanvas.getContext("2d");
+
+const CANVAS_WIDTH = (myCanvas.width = 100);
+const CANVAS_HEIGHT = (myCanvas.height = 100);
+
+const myImg = document.getElementById("myImg");
+const spriteWidth = 88;
+const spriteHeight = 88;
+let gameFrame = 0;
+let stagger = 0;
+const staggerFrame = 10;
+
+function animate() {
+  console.log("Stagger: " + stagger);
+
+  if (stagger == staggerFrame) {
+    console.log("StaggerFrame: " + staggerFrame);
+
+    if (gameFrame < 6) {
+      // c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+      //   change game frame automatically
+      let frameX = spriteWidth * gameFrame;
+      let frameY = spriteHeight * gameFrame;
+
+      c.drawImage(
+        myImg,
+        frameX,
+        frameY,
+        spriteWidth,
+        spriteHeight,
+        0,
+        0,
+        CANVAS_WIDTH,
+        CANVAS_HEIGHT
+      );
+      gameFrame++;
+    } else {
+      gameFrame = 0;
+    }
+    stagger = 0;
+  }
+  stagger++;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
